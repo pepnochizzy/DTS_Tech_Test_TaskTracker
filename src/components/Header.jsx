5880551b671e8a16bd1c2c780114922b54806fb1
pepnochizzy <laragray@live.co.uk>
2026-04-07 01:08:26 +0100

    feat: changed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <nav className="flex flex-row place-content-around bg-black text-white h-12 items-center">
       <Link href={"/"}>
-        <p className="font-bold text-2xl">GOV.UK</p>
+        <p className="font-bold text-2xl">TASK.UK</p>
       </Link>
       <p className="font-bold">Task Tracker</p>
     </nav>
